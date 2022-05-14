fda5786d0339a72c1f7e3c1413a11b95cddc0b8a
Jatin <jatin.parate@mlveda.com>
2022-05-14 17:03:09 +0530

    Added post product e2e tests

--- test/product.e2e-spec.ts
+++ test/product.e2e-spec.ts
@@ -3,11 +3,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { Repository } from 'typeorm';
-import { User } from '../src/users/entities/user';
+import { Roles, User } from '../src/users/entities/user';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Product } from '../src/products/entities/product';
 import faker from '@faker-js/faker';
 import { AuthService } from '../src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('ProductController (e2e)', () => {
   let app: INestApplication;
@@ -16,6 +17,7 @@ describe('ProductController (e2e)', () => {
   let user: User;
   let userData: Pick<User, 'email' | 'password' | 'name'>;
   let authService: AuthService;
+  let jwtService: JwtService;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -29,9 +31,11 @@ describe('ProductController (e2e)', () => {
     await app.init();
     authService = moduleFixture.get<AuthService>(AuthService);
     usersRepo = moduleFixture.get<Repository<User>>(getRepositoryToken(User));
+    jwtService = moduleFixture.get<JwtService>(JwtService);
     productsRepo = moduleFixture.get<Repository<Product>>(
       getRepositoryToken(Product),
     );
+    await usersRepo.clear();
     userData = {
       email: faker.internet.email(),
       password: faker.internet.password(),
@@ -122,6 +126,70 @@ describe('ProductController (e2e)', () => {
       for (const product of response.body) {
         expect(product.name).toBe(products[0].name);
       }
+    });
+  });
+
+  describe('/ (POST)', () => {
+    let token: string;
+    let adminUser: User;
+    let adminToken: string;
+
+    beforeAll(async () => {
+      adminUser = await usersRepo.save({
+        email: faker.internet.email(),
+        name: faker.internet.userName(),
+        password: await authService.hashPassword(userData.password),
+        role: Roles.ADMIN,
+      });
+      adminToken = jwtService.sign({
+        email: adminUser.email,
+        id: adminUser.id,
+        role: Roles.ADMIN,
+      });
+      token = jwtService.sign({
+        email: user.email,
+        id: user.id,
+        role: user.role,
+      });
+    });
+
+    it('should fail if not authorized', async () => {
+      await request(app.getHttpServer())
+        .post('/products')
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('should fail if user is not admin', async () => {
+      await request(app.getHttpServer())
+        .post('/products')
+        .set('Authorization', `Bearer ${token}`)
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('should fail if user is admin and no product is provided', async () => {
+      await request(app.getHttpServer())
+        .post('/products')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send()
+        .expect(HttpStatus.BAD_REQUEST);
+    });
+
+    it('should create product in db', async () => {
+      const productData: Partial<Product> = {
+        name: faker.commerce.productName(),
+        price: faker.datatype.number({ min: 0 }),
+        quantity: faker.datatype.number({ min: 0 }),
+        description: faker.lorem.sentence(),
+      };
+      await request(app.getHttpServer())
+        .post('/products')
+        .set('Authorization', `Bearer ${adminToken}`)
+        .send(productData)
+        .expect(HttpStatus.CREATED);
+      await expect(productsRepo.findOne(productData)).resolves.toMatchObject({
+        ...productData,
+        isActive: false,
+      });
     });
   });
 });
